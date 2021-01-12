256675a4016599b68855c37c56117c61bb833ff6
Monia <monia@pcs-macbook-pro.home>
2021-01-12 17:04:05 +0100

    display users public IP on map

--- js/UserMapMarker.js
+++ js/UserMapMarker.js
@@ -1,36 +1,19 @@
 class UserMapMarker {
   constructor(){
-    this.publicIp = localStorage.getItem("user-ip");
-    this.url = `https://geo.ipify.org/api/v1?apiKey=at_SiB15INYsXcVuNNK1vZcN0D2dNIHG&ipAddress=${this.publicIp}`
-    this.userIPinput = document.querySelector(".header__user-ip");
-    this.ip = document.querySelector(".ip");
-    this.location = document.querySelector(".location");
-    this.timezone = document.querySelector(".time");
-    this.isp = document.querySelector(".isp");
-    this.event();
+    this.lat = localStorage.getItem("lat");
+    this.lng = localStorage.getItem("lng");
+    this.event()
   }
 
   event() {
-      window.addEventListener("load", () => this.getUserData());
+      window.addEventListener("load", () => this.loadMap());
     }
 
-  getUserData() {
-   fetch(this.url)
-       .then(response => response.json())
-       .then((data => {
-         console.log(data)
-         this.userIPinput.textContent = `Your IP is ${this.publicIp}`;
-         this.ip.textContent = this.publicIp;
-         this.location.textContent = data.location.city;
-         this.timezone.textContent = `UTC ${data.location.timezone}`;
-         this.isp.textContent = data.isp;
-         localStorage.setItem('lat', data.location.lat);
-         localStorage.setItem('lng', data.location.lng);
-
-       }))
-       .catch(err => {
-         console.log(err)
-       })
+  loadMap() {
+    console.log(this.lat, this.lng)
+    const map = L.map('mapid').setView([54.2, this.lng], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar', attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'}).addTo(map);
+    var marker = L.marker([this.lat, this.lng]).addTo(map);
   }
 }
 
--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 import '../sass/style.scss';
 import UserIP from '../js/UserIP.js';
 import UserLocation from '../js/UserLocation.js';
+import UserMapMarker from '../js/UserMapMarker.js';
 
 const userIP = new UserIP;
 const userLocation = new UserLocation;
+const userMapMarker = new UserMapMarker;
 
