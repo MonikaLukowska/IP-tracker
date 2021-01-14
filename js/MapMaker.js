20e7783581e7b7b84eb92eb94d16df08a04ffe37
Monia <monia@pcs-macbook-pro.home>
2021-01-14 20:51:56 +0100

    add custom marker

--- js/MapMaker.js
+++ js/MapMaker.js
@@ -2,13 +2,17 @@ class MapMaker {
   constructor(){
     this.lat = localStorage.getItem('lat');
     this.lng = localStorage.getItem('lng');
+    this.icon = L.icon ({
+      iconUrl: '../icon-location.svg',
+      iconSize: [20, 20]
+    })
   }
   
   loadMapFirst(map) {
     
     map.setView([this.lat, this.lng], 13);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar', attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'}).addTo(map);
-    L.marker([this.lat, this.lng]).addTo(map)
+    L.marker([this.lat, this.lng], {icon:this.icon}).addTo(map)
     }
 
   
